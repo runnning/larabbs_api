a612dc9be8931232f60b502e5eb3cb84c550410c
yanWN <2467634970@qq.com>
2021-08-23 11:23:22 +0800

    话题详情

---
+++ app/Http/Queries/TopicQuery.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Queries;
+
+use App\Models\Topic;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class TopicQuery extends QueryBuilder
+{
+    public function __construct()
+    {
+        parent::__construct(Topic::query());
+
+        $this->allowedIncludes('user','category')
+            ->allowedFilters([
+                'title',
+                AllowedFilter::exact('category_id'),
+                AllowedFilter::scope('withOrder')->default('recentReplied'),
+            ]);
+    }
+}
