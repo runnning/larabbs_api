dc5a68a4c67c35e30a237612825c0b1808f5e0c5
yanWN <2467634970@qq.com>
2021-08-24 12:53:21 +0800

    回复列表

--- routes/api.php
+++ routes/api.php
@@ -73,6 +73,12 @@ Route::prefix('v1')
                 Route::resource('topics',TopicsController::class)->only([
                     'index','show'
                 ]);
+                //话题回复列表
+                Route::get('topics/{topic}/replies',[RepliesController::class,'index'])
+                    ->name('topics.replies.index');
+                //某个用户的回复列表
+                Route::get('users/{user}/replies',[RepliesController::class,'userIndex'])
+                    ->name('users.replies.index');
                 //登录后可以访问的接口
                 Route::middleware('auth:api')->group(function (){
                     //当前登录用户信息
